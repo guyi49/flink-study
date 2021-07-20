e6beb19b5d5e5d9a9ec13cb04e0cbf3fd08744f7
guyi49 <853779011@qq.com>
2021-07-21 00:08:09 +0800

    序列化操作

--- hadoop-train/src/main/java/com/guyi/hadoop/io/Person.java
+++ hadoop-train/src/main/java/com/guyi/hadoop/io/Person.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 /*
  * 序列化实体类
  */
-
 public class Person implements WritableComparable <Person>{
     private Text name = new Text();
     private IntWritable age = new IntWritable();
@@ -36,16 +35,74 @@ public class Person implements WritableComparable <Person>{
         this.sex.set(sex);
     }
 
-
+    // 比较规则：姓名相同比年龄，年龄相同比性别
     public int compareTo(Person o) {
-        return 0;
+        int res = 0;
+        int com1 = name.compareTo(o.name);
+        if(com1 !=0 ){
+            return com1;
+        }
+        int com2 =age.compareTo(o.age);
+        if (com2 !=0 ){
+            return com2;
+        }
+        int com3 =sex.compareTo(o.sex);
+        if (com3 !=0 ){
+            return com3;
+        }
+
+        return res;
     }
 
     public void write(DataOutput dataOutput) throws IOException {
-
+        name.write(dataOutput);
+        age.write(dataOutput);
+        sex.write(dataOutput);
     }
 
     public void readFields(DataInput dataInput) throws IOException {
+        name.readFields(dataInput);
+        age.readFields(dataInput);
+        sex.readFields(dataInput);
+    }
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((age ==null)?0:age.hashCode());
+        result = prime * result + ((name ==null)?0:name.hashCode());
+        result = prime * result + ((sex ==null)?0:sex.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (age == null){
+            if (other.age != null)
+                return false;
+        }else if (!age.equals(other.age))
+            return false;
+        if (name == null){
+            if (other.name != null)
+                return false;
+        }else if (!name.equals(other.name))
+            return false;
+        if (sex == null){
+            if (other.sex != null)
+                return false;
+        }else if (!sex.equals(other.sex))
+            return false;
+        return true;
+    }
 
+    @Override
+    public String toString(){
+        return "Person [name = "+ name+",age =  " + age + ",sex = "+sex+"]";
     }
 }
